796e34af743331ae9325a11e00c9b90d1553d7cf
SeyiAkinwale1 <20927904-SeyiAkinwale1@users.noreply.replit.com>
2023-04-08 05:26:46 +0000

    recursive case

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 using namespace std;
 
-int mult(int MULTIPLICAND1, int multiplicand2)
+int mult(const int MULTIPLICAND1, int multiplicand2)
 {
-  if(multiplicand2==2) return (MULTIPLICAND1+MULTIPLICAND2);
+  if(multiplicand2==2) return (MULTIPLICAND1+MULTIPLICAND1);
+  else return MULTIPLICAND1+ mult(MULTIPLICAND1, multiplicand2-1);
 }
 int main() {
   cout << "Hello World!\n";
-  cout << end; mult(5,2);
+  cout << endl << mult(5,2);
+  cout << endl << mult(5,3);
 }
