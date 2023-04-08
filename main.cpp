9ad3ee8f9ff930e2db99008b0bba7970c858ef1a
SeyiAkinwale1 <20927904-SeyiAkinwale1@users.noreply.replit.com>
2023-04-08 05:29:26 +0000

    more tests

--- main.cpp
+++ main.cpp
@@ -3,11 +3,14 @@ using namespace std;
 
 int mult(const int MULTIPLICAND1, int multiplicand2)
 {
-  if(multiplicand2==) return MULTIPLICAND1;
+  if(multiplicand2==1) return MULTIPLICAND1;
   else return MULTIPLICAND1+ mult(MULTIPLICAND1, multiplicand2-1);
 }
 int main() {
   cout << "Hello World!\n";
+  cout << endl << mult(5,1);
   cout << endl << mult(5,2);
   cout << endl << mult(5,6);
+  cout << endl << mult(2,7);
+  cout << endl << mult(1,7);
 }
