3780779227398ff650fee96f3c7bfabd98d56c14
SeyiAkinwale1 <20927904-SeyiAkinwale1@users.noreply.replit.com>
2023-04-08 05:18:55 +0000

    base case

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 using namespace std;
 
+int mult(int MULTIPLICAND1, int multiplicand2)
+{
+  if(multiplicand2==2) return (MULTIPLICAND1+MULTIPLICAND2);
+}
 int main() {
   cout << "Hello World!\n";
+  cout << end; mult(5,2);
 }
