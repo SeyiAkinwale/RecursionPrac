0b77a5eae71507a1435ac912a0a2d3cf663c3a3d
SeyiAkinwale1 <20927904-SeyiAkinwale1@users.noreply.replit.com>
2023-04-08 05:27:42 +0000

    reduced base case

--- main.cpp
+++ main.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 int mult(const int MULTIPLICAND1, int multiplicand2)
 {
-  if(multiplicand2==2) return (MULTIPLICAND1+MULTIPLICAND1);
+  if(multiplicand2==) return MULTIPLICAND1;
   else return MULTIPLICAND1+ mult(MULTIPLICAND1, multiplicand2-1);
 }
 int main() {
   cout << "Hello World!\n";
   cout << endl << mult(5,2);
-  cout << endl << mult(5,3);
+  cout << endl << mult(5,6);
 }
