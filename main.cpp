4e95c01823ac6d99b50a6e0ff210ead94c979b12
SeyiAkinwale1 <72584469+SeyiAkinwale@users.noreply.github.com>
2023-04-09 05:50:01 +0000

    hello1

--- main.cpp
+++ main.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int mult(const int MULTIPLICAND1, int multiplicand2)
+int getDigitCount(int num, int digit)
 {
-  if(multiplicand2==1) return MULTIPLICAND1;
-  else return MULTIPLICAND1+ mult(MULTIPLICAND1, multiplicand2-1);
+    return -1;  // This is incorrect.
 }
+
 int main() {
   cout << "Hello World!\n";
-  cout << endl << mult(5,1);
-  cout << endl << mult(5,2);
-  cout << endl << mult(5,6);
-  cout << endl << mult(2,7);
-  cout << endl << mult(1,7);
+  cout << "hello1";
+  getDigitCount(12235,2);
+  cout << endl << 12/10;
+  cout << endl << 12%10;
+  cout << endl << 1/10;
+  cout << endl << 1%10;
 }
