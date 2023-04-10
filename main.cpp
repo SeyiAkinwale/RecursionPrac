ebc06ebcdd0daffcac53022cd23d7dcf787fc757
SeyiAkinwale1 <72584469+SeyiAkinwale@users.noreply.github.com>
2023-04-10 18:30:59 +0000

    checking for digit

--- main.cpp
+++ main.cpp
@@ -2,20 +2,19 @@
 using namespace std;
 
 int getDigitCount(int num, int digit) {
-  if (num <= 10) return 1;
-  else return (getDigitCount(num/10,2)+1);
-  //return -1; // This is incorrect.
+  if (num <= 10 && num==digit) 
+    return 1;
+  else if (num <=10 && num!=digit) 
+    return 0;
+  else if (num%10==digit) 
+    return (getDigitCount(num/10, digit)+1);
+  else
+    return getDigitCount(num/10, digit);
 }
 
 int main() {
-  cout << "Hello World!\n";
-  cout << "hello1";
-  getDigitCount(12235, 2);
-  cout << endl << 12 / 10;
-  cout << endl << 12 % 10;
-  cout << endl << 1 / 10;
-  cout << endl << 1 % 10;
-  int number = 10;
-  cout <<endl<<number;
-  cout<< endl<< sizeof(number);
+  cout<< endl<< getDigitCount(12235, 2);
+  cout<< endl<< getDigitCount(12235, 7);
+  cout<< endl<< getDigitCount(12235, 2);
+  return 0;
 }
