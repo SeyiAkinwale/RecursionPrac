4513637403b7fdd25a42c6a8772eac6ba30df4da
SeyiAkinwale1 <72584469+SeyiAkinwale@users.noreply.github.com>
2023-04-10 19:37:54 +0000

    dubleDollars

--- .ccls-cache/@home@runner@RecursionPrac/main.cpp
+++ .ccls-cache/@home@runner@RecursionPrac/main.cpp
@@ -1,22 +1,12 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int getDigitCount(int num, int digit) {
-  if (num <= 10 && num==digit) 
-    return 1;              //primary digit that is target
-  else if (num <=10 && num!=digit) 
-    return 0;              //primary digit that is not target
-  else if (num%10==digit)  //non-primary digit that is target
-    return (getDigitCount(num/10, digit)+1); 
-  else                     //non-primary digit that is not target
-    return getDigitCount(num/10, digit);
+string doubleDollars(string n) {
+  return ""; //
 }
 
 int main() {
-  cout<< endl<< getDigitCount(12235, 2);
-  cout<< endl<< getDigitCount(12235, 7);
-  cout<< endl<< getDigitCount(12235, 2);
-  cout<< endl<< getDigitCount(0, 2);
-  cout<< endl<< getDigitCount(0, 0);
+  cout << endl << doubleDollars("goodbye");
   return 0;
 }
