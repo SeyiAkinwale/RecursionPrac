f319d679b21b31112e38558c1e30caf56b5a3b86
SeyiAkinwale1 <72584469+SeyiAkinwale@users.noreply.github.com>
2023-04-10 18:38:19 +0000

    testing

--- main.cpp
+++ main.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int getDigitCount(int num, int digit) {
   if (num <= 10 && num==digit) 
-    return 1;
+    return 1;              //primary digit that is target
   else if (num <=10 && num!=digit) 
-    return 0;
-  else if (num%10==digit) 
-    return (getDigitCount(num/10, digit)+1);
-  else
+    return 0;              //primary digit that is not target
+  else if (num%10==digit)  //non-primary digit that is target
+    return (getDigitCount(num/10, digit)+1); 
+  else                     //non-primary digit that is not target
     return getDigitCount(num/10, digit);
 }
 
@@ -16,5 +16,6 @@ int main() {
   cout<< endl<< getDigitCount(12235, 2);
   cout<< endl<< getDigitCount(12235, 7);
   cout<< endl<< getDigitCount(12235, 2);
+  cout<< endl<< getDigitCount(0, 2);
   return 0;
 }
