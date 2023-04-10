3d2ff48bcedf9b93c433ccbe605b9779c313bd51
SeyiAkinwale1 <72584469+SeyiAkinwale@users.noreply.github.com>
2023-04-10 17:14:40 +0000

    counting all digits

--- main.cpp
+++ main.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int getDigitCount(int num, int digit)
-{
-    return -1;  // This is incorrect.
+int getDigitCount(int num, int digit) {
+  if (num <= 10) return 1;
+  else return (getDigitCount(num/10,2)+1);
+  //return -1; // This is incorrect.
 }
 
 int main() {
   cout << "Hello World!\n";
   cout << "hello1";
-  getDigitCount(12235,2);
-  cout << endl << 12/10;
-  cout << endl << 12%10;
-  cout << endl << 1/10;
-  cout << endl << 1%10;
+  getDigitCount(12235, 2);
+  cout << endl << 12 / 10;
+  cout << endl << 12 % 10;
+  cout << endl << 1 / 10;
+  cout << endl << 1 % 10;
+  int number = 10;
+  cout <<endl<<number;
+  cout<< endl<< sizeof(number);
 }
