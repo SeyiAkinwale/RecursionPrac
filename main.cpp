2371b14bead46a747f087349a892f3c68186d3a5
SeyiAkinwale1 <72584469+SeyiAkinwale@users.noreply.github.com>
2023-04-11 05:57:47 +0000

    can handle 2 repeats

--- main.cpp
+++ main.cpp
@@ -5,7 +5,10 @@ using namespace std;
 string doubleDollars(string n) 
 {
 
-  if(n.length()==2) return n.substr(0,2);
+  if(n.length()==2 && n[n.length()-2]!=n[n.length()-1])
+    return n.substr(0,2);
+  else if(n.length()==2 && (n[n.length()-2]==n[n.length()-1]))
+    return (n.substr(0,1)+"$$"+n.substr(1,1));
   else if (n[n.length()-2]==n[n.length()-1])
   {
     return doubleDollars(n.substr(0,n.length()-1))+"$$"+n[n.length()-1];
@@ -20,6 +23,8 @@ string doubleDollars(string n)
 
 int main() {
   cout << endl << doubleDollars("goodbye");
+  cout << endl << doubleDollars("yy");
   cout << endl << doubleDollars("yyuu");
+  cout << endl << doubleDollars("aaaa");
   return 0;
 }
