655ee6eafccc0d2b6d55c2048f5bd43fdfe56196
SeyiAkinwale1 <72584469+SeyiAkinwale@users.noreply.github.com>
2023-04-11 04:32:08 +0000

    string concatenation

--- main.cpp
+++ main.cpp
@@ -5,8 +5,14 @@ using namespace std;
 string doubleDollars(string n) {
   //cout<<endl<<n.length();
   //cout<<endl<<n.substr(0,n.length()-2);
-  if (n[n.length()-1]==n[n.length()]) cout<<"\n$";
-  return ""; //
+  if(n.length()==1) return "";
+  else if (n[n.length()-1]==n[n.length()])
+  {
+    return doubleDollars(n.substr(0,n.length()-1))+n[n.length()];
+    cout<<"hi";
+  }
+  //if (n[n.length()-1]==n[n.length()]) cout<<"\n$";
+  //return ""; //
 }
 
 int main() {
