ebe41537911fb09b90c32abf590c6659c0cba526
SeyiAkinwale1 <72584469+SeyiAkinwale@users.noreply.github.com>
2023-04-11 18:35:08 +0000

    single less than sign

--- main.cpp
+++ main.cpp
@@ -4,15 +4,15 @@ using namespace std;
 
 string findAngles(string str) 
 {
-  if(str.length()==1 && str[0]=="<")
+  if(str.length()==1 && str[0]=='k')
     return "<";
-  else if(str.length()==1 && str[0]!="<")
+  else if(str.length()==1 && str[0]!='<')
     return "";
 }
 
 
 int main() {
-  cout << endl << findAngles("a");
+  cout << endl << findAngles("<");
 
   return 0;
 }
