700a6fa3086dc6c203db3d5c57b80602788fd9f1
SeyiAkinwale1 <72584469+SeyiAkinwale@users.noreply.github.com>
2023-04-11 06:00:01 +0000

    straightenin

--- main.cpp
+++ main.cpp
@@ -4,20 +4,14 @@ using namespace std;
 
 string doubleDollars(string n) 
 {
-
   if(n.length()==2 && n[n.length()-2]!=n[n.length()-1])
     return n.substr(0,2);
   else if(n.length()==2 && (n[n.length()-2]==n[n.length()-1]))
     return (n.substr(0,1)+"$$"+n.substr(1,1));
   else if (n[n.length()-2]==n[n.length()-1])
-  {
     return doubleDollars(n.substr(0,n.length()-1))+"$$"+n[n.length()-1];
-    //return doubleDollars(n.substr(0,n.length()-1))+n[n.length()-1];
-  }else if (n[n.length()-2]!=n[n.length()-1])
-  {
+  else if (n[n.length()-2]!=n[n.length()-1])
     return doubleDollars(n.substr(0,n.length()-1))+n[n.length()-1];
-  }
-
 }
 
 
