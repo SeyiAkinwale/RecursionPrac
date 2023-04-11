db8f14deeba41c2c8f3a14ceb70d3e29a0a298d0
SeyiAkinwale1 <72584469+SeyiAkinwale@users.noreply.github.com>
2023-04-11 21:39:51 +0000

    characters within less than signs

--- main.cpp
+++ main.cpp
@@ -11,12 +11,13 @@ string findAngles(string str)
   //else if (str.length()!=1 && str[str.length()-1]=='<')
   //  return "<";
   else if (str[str.length()-1]=='>')
-    return findAngles(str.substr(str.length()-1))+">";
+    return findAngles(str.substr(0,str.length()-1))+">";
+  else return findAngles(str.substr(0,str.length()-1))+str[str.length()-1];
 }
 
 
 int main() {
-  cout << endl << findAngles("<");
+  cout << endl << findAngles("<>");
 
   return 0;
 }
