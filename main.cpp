c0c6c6f2796089928a063e2efa4324e80192cc49
SeyiAkinwale1 <72584469+SeyiAkinwale@users.noreply.github.com>
2023-04-11 04:41:45 +0000

    length()-2  -- length()-1

--- main.cpp
+++ main.cpp
@@ -5,10 +5,15 @@ using namespace std;
 string doubleDollars(string n) {
   //cout<<endl<<n.length();
   //cout<<endl<<n.substr(0,n.length()-2);
+  //cout<<n[n.length()-1];
   if(n.length()==1) return "";
-  else if (n[n.length()-1]==n[n.length()])
+  else if (n[n.length()-2]==n[n.length()-1])
   {
-    return doubleDollars(n.substr(0,n.length()-1))+n[n.length()];
+    return doubleDollars(n.substr(0,n.length()-2))+n[n.length()-1];
+    cout<<"hi";
+  }else if (n[n.length()-2]!=n[n.length()-1])
+  {
+    return doubleDollars(n.substr(0,n.length()-2))+n[n.length()-1];
     cout<<"hi";
   }
   //if (n[n.length()-1]==n[n.length()]) cout<<"\n$";
