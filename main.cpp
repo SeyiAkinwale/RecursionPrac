0bb37507b425fe0e23b952573ffe275bcabcee76
SeyiAkinwale1 <72584469+SeyiAkinwale@users.noreply.github.com>
2023-04-11 19:55:15 +0000

    moving in to 2 characters

--- main.cpp
+++ main.cpp
@@ -4,15 +4,17 @@ using namespace std;
 
 string findAngles(string str) 
 {
-  if(str.length()==1 && str[0]=='k')
+  if(str.length()==1 && str[0]=='<')
     return "<";
   else if(str.length()==1 && str[0]!='<')
     return "";
+  else if (str.length()>1)
+    return findAngles(str.substr(0,str.length()-1))+str[str.length()-1];
 }
 
 
 int main() {
-  cout << endl << findAngles("<");
+  cout << endl << findAngles("aa");
 
   return 0;
 }
