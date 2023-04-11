000fa26aa818ebb06543897d2e8dceca55cecd40
SeyiAkinwale1 <72584469+SeyiAkinwale@users.noreply.github.com>
2023-04-11 05:46:57 +0000

    workhs for one repetition

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ string doubleDollars(string n)
   if(n.length()==2) return n.substr(0,2);
   else if (n[n.length()-2]==n[n.length()-1])
   {
-    return doubleDollars(n.substr(0,n.length()-1))+"$$";
+    return doubleDollars(n.substr(0,n.length()-1))+"$$"+n[n.length()-1];
     //return doubleDollars(n.substr(0,n.length()-1))+n[n.length()-1];
   }else if (n[n.length()-2]!=n[n.length()-1])
   {
@@ -20,6 +20,6 @@ string doubleDollars(string n)
 
 int main() {
   cout << endl << doubleDollars("goodbye");
-  //cout << endl << "hello" +"k";
+  cout << endl << doubleDollars("yyuu");
   return 0;
 }
