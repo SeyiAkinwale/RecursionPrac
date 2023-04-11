8896cb7240861f2450fa9b7759a4c515c7b9876c
SeyiAkinwale1 <72584469+SeyiAkinwale@users.noreply.github.com>
2023-04-11 21:34:16 +0000

    greater than sign

--- main.cpp
+++ main.cpp
@@ -4,17 +4,19 @@ using namespace std;
 
 string findAngles(string str) 
 {
-  if(str.length()==1 && str[0]=='<')
+  if(str[str.length()-1]=='<')
     return "<";
   else if(str.length()==1 && str[0]!='<')
     return "";
-  else if (str.length()>1)
-    return findAngles(str.substr(0,str.length()-1))+str[str.length()-1];
+  //else if (str.length()!=1 && str[str.length()-1]=='<')
+  //  return "<";
+  else if (str[str.length()-1]=='>')
+    return findAngles(str.substr(str.length()-1))+">";
 }
 
 
 int main() {
-  cout << endl << findAngles("aa");
+  cout << endl << findAngles("<");
 
   return 0;
 }
