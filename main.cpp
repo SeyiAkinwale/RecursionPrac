ed21606a5a8b4d5689ec4354f21c8c20e08b1f76
SeyiAkinwale1 <72584469+SeyiAkinwale@users.noreply.github.com>
2023-04-11 18:24:15 +0000

    empty string

--- main.cpp
+++ main.cpp
@@ -2,23 +2,15 @@
 #include <string>
 using namespace std;
 
-string doubleDollars(string n) 
+string findAngles(string str) 
 {
-  if(n.length()==2 && n[n.length()-2]!=n[n.length()-1])
-    return n.substr(0,2);
-  else if(n.length()==2 && (n[n.length()-2]==n[n.length()-1]))
-    return (n.substr(0,1)+"$$"+n.substr(1,1));
-  else if (n[n.length()-2]==n[n.length()-1])
-    return doubleDollars(n.substr(0,n.length()-1))+"$$"+n[n.length()-1];
-  else if (n[n.length()-2]!=n[n.length()-1])
-    return doubleDollars(n.substr(0,n.length()-1))+n[n.length()-1];
+  if(str.length()==0)
+    return "";
 }
 
 
 int main() {
-  cout << endl << doubleDollars("goodbye");
-  cout << endl << doubleDollars("yy");
-  cout << endl << doubleDollars("yyuu");
-  cout << endl << doubleDollars("aaaa");
+  cout << endl << findAngles("");
+
   return 0;
 }
