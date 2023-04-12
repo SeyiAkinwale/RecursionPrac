035f9cdaaaf02fe8358f151de7e9ab701ef1b052
SeyiAkinwale1 <72584469+SeyiAkinwale@users.noreply.github.com>
2023-04-12 02:09:09 +0000

    findAngles solved

--- main.cpp
+++ main.cpp
@@ -4,22 +4,30 @@ using namespace std;
 
 string findAngles(string str) 
 {
-  if(str[0]=='<' && str[str.length()-1]=='>')
-    return str;
+  //base case: both left and right elem are brackets
+  if(str[0]=='<' && str[str.length()-1]=='>') return str;
+  
+  //recursive case 1: only the left elem is bracket
+  //so, remove last element
   else if (str[0]=='<')
     return findAngles(str.substr(0,str.length()-1));
+  
+  //recursive case 2: only the right elem is bracket
+  //so, remove first element
   else if (str[str.length()-1]=='>')
     return findAngles(str.substr(1,str.length()-1));
-  else
-      return findAngles(str.substr(1,str.length()-2));
+  
+  //recursive case 3: neither elem is bracket
+  //so, remove both ends
+  else return findAngles(str.substr(1,str.length()-2));
 }
-
 
 int main() {
   cout << endl << findAngles("a<aaafg>a");
-  cout << endl << findAngles("abs<bnm>789");
-  //cout << endl << findAngles("4agh<y>");
-  //cout << endl << findAngles("<a>bbbbbb");
+  cout << endl << findAngles("abc<bnm>789");
+  cout << endl << findAngles("4agh<y>");
+  cout << endl << findAngles("<x>7");
+  cout << endl << findAngles("<a>bbbbbb");
 
   return 0;
 }
