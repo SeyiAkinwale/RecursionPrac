26ee388268231777b11095837ec5d377d74cd2ca
SeyiAkinwale1 <72584469+SeyiAkinwale@users.noreply.github.com>
2023-04-12 01:00:42 +0000

    how to wait for opening brace?

--- main.cpp
+++ main.cpp
@@ -12,12 +12,18 @@ string findAngles(string str)
   //  return "<";
   else if (str[str.length()-1]=='>')
     return findAngles(str.substr(0,str.length()-1))+">";
-  else return findAngles(str.substr(0,str.length()-1))+str[str.length()-1];
+  //else if ()
+   // return "";
+  else 
+    return findAngles(str.substr(0,str.length()-1))+str[str.length()-1];
 }
 
 
 int main() {
-  cout << endl << findAngles("<>");
+  cout << endl << findAngles("a<aaafg>");
+  cout << endl << findAngles("abs<bnm>789");
+  cout << endl << findAngles("4agh<y>");
+  cout << endl << findAngles("<a>bbbbbb");
 
   return 0;
 }
