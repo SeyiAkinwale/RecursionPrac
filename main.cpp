3c4d9fa995bc5ece95ae430e3db41b8a928ff084
SeyiAkinwale <72584469+SeyiAkinwale@users.noreply.github.com>
2023-04-18 16:54:33 +0000

    base cases setup
    base cases setup

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
-string findAngles(string str) 
+canEqualTarget(const int a[], int size, int target)
 {
   //base case: both left and right elem are brackets
   if(str[0]=='<' && str[str.length()-1]=='>') return str;
@@ -11,23 +10,12 @@ string findAngles(string str)
   //so, remove last element
   else if (str[0]=='<')
     return findAngles(str.substr(0,str.length()-1));
-  
-  //recursive case 2: only the right elem is bracket
-  //so, remove first element
-  else if (str[str.length()-1]=='>')
-    return findAngles(str.substr(1,str.length()-1));
-  
-  //recursive case 3: neither elem is bracket
-  //so, remove both ends
-  else return findAngles(str.substr(1,str.length()-2));
+
 }
 
 int main() {
   cout << endl << findAngles("a<aaafg>a");
-  cout << endl << findAngles("abc<bnm>789");
-  cout << endl << findAngles("4agh<y>");
-  cout << endl << findAngles("<x>7");
-  cout << endl << findAngles("<a>bbbbbb");
+
 
   return 0;
 }
